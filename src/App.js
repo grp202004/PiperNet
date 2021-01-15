a1d586bc551f07e03ee7cecce42ebcb7055210d4
samsadadsadsa <1144125848@qq.com>
2021-01-15 14:49:54 +0800

    update panels
    fix bugs in edgespanel and labelspanel, but they still have bugs :  (labelpanel) select, 4 handlers, (edgespanel): sketchpicker

--- src/App.js
+++ src/App.js
@@ -12,23 +12,29 @@ import Dialogs from "./components/Dialogs";
 import ThreeJSVis from "./components/visualize/visualizer";
 // import NodeDetail from "./components/panels/NodeDetailPanel";
 import State from "./state/index";
+import PopoverExample from "./components/test";
+import EdgesPanel from "./components/panels/EdgesPanel";
+import LabelsPanel from "./components/panels/LabelsPanel";
 // import FloatingCards from "./components/FloatingCards";
 
 function App() {
     return (
         <div
-            className={classnames({
-                "app-wrapper": true,
-                [Classes.DARK]: true,
-            })}
+            // className={classnames({
+            //     "app-wrapper": true,
+            //     [Classes.DARK]: true,
+            // })}
         >
-            <Navbar />
-            <main className="main">
-                <ThreeJSVis />
-            </main>
+            {/* <Navbar /> */}
+            {/* <main className="main"> */}
+                {/* <ThreeJSVis /> */}
+            {/* </main> */}
             {/* {State.graph.hasGraph && <FloatingCards />} */}
-            <Dialogs />
+            {/* <Dialogs /> */}
+            {/* <PopoverExample/> */}
+            <LabelsPanel/>
         </div>
+
     );
 }
 
